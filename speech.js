16fbd493910df0415a59c62369c0c3e868a476f4
Jimmy-Qattan <59592522+Jimmy-Qattan@users.noreply.github.com>
2026-04-19 09:35:55 -0700

    Create speech.js

---
+++ speech.js
@@ -0,0 +1,73 @@
+const URL = "https://teachablemachine.withgoogle.com/models/NERV5aBDP/";
+
+let commandingOn = false;
+
+async function createModel() {
+  const checkpointURL = URL + "model.json"; // model topology
+  const metadataURL = URL + "metadata.json"; // model metadata
+
+  const recognizer = speechCommands.create(
+    "BROWSER_FFT", // fourier transform type, not useful to change
+    undefined, // speech commands vocabulary feature, not useful for your models
+    checkpointURL,
+    metadataURL,
+  );
+
+  // check that model and metadata are loaded via HTTPS requests.
+  await recognizer.ensureModelLoaded();
+
+  return recognizer;
+}
+
+async function init() {
+  const recognizer = await createModel();
+  const classLabels = recognizer.wordLabels(); // get class labels
+  // for (let i = 0; i < classLabels.length; i++) {
+  //     labelContainer.appendChild(document.createElement("div"));
+  // }
+
+  // listen() takes two arguments:
+  // 1. A callback function that is invoked anytime a word is recognized.
+  // 2. A configuration object with adjustable fields
+  recognizer.listen(
+    (result) => {
+      const scores = result.scores; // probability of prediction for each class
+      // render the probability scores per class
+      for (let i = 0; i < classLabels.length; i++) {
+        const classPrediction =
+          classLabels[i] + ": " + result.scores[i].toFixed(2);
+        // labelContainer.childNodes[i].innerHTML = classPrediction;
+
+        if (result.scores[i].toFixed(2) >= 0.6 && commandingOn) {
+          window.dispatchEvent(
+            new CustomEvent("voiceCommand", { detail: classLabels[i] }),
+          );
+
+          console.log("There should be something on screen now");
+        }
+      }
+    },
+    {
+      includeSpectrogram: true, // in case listen should return result.spectrogram
+      probabilityThreshold: 0.75,
+      invokeCallbackOnNoiseAndUnknown: true,
+      overlapFactor: 0.5, // probably want between 0.5 and 0.75. More info in README
+    },
+  );
+
+  // Stop the recognition in 5 seconds.
+  // setTimeout(() => recognizer.stopListening(), 5000);
+}
+
+const button = document.getElementById("enableVoiceCommanding");
+
+button.addEventListener("click", function () {
+  init();
+
+  commandingOn
+    ? (button.innerHTML = "Enable Voice")
+    : (button.innerHTML = "Disable Voice");
+
+  commandingOn = !commandingOn;
+  console.log(commandingOn);
+});
